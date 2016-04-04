85e457bf8829d6470fbee1a43f31c05e5f586945
leasbenmaupa <leasbenmaupa@gmail.com>
2016-04-04 14:35:47 +0200

    Update boss.php

--- leesim/boss.php
+++ leesim/boss.php
@@ -5,10 +5,22 @@
 </head>
 <body>
 <?php
-$conn=mysql_connect('localhost','root','tavonga1');
+$conn=mysql_connect('localhost','root','password');
 if(!$conn){
-	die("Connection Error");
+	die("Connection Error: " .mysql_error());
 }
+mysql_select_db("trust",$conn);
+
+$sql="CREATE TABLE(
+	id int(8) primary key auto_increment,
+	firstname varchar(30),
+	lastname varchar(40),
+	address varchar(20)
+	)";
+	
+mysql_query($sql,$conn);
+
+mysql_close($conn);
 ?>
 </body>
 </html>
