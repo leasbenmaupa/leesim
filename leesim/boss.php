b2e5cee0ed1273316b58d4006634c2e012aa3fe7
leasbenmaupa <leasbenmaupa@gmail.com>
2016-04-04 13:59:13 +0200

    First Commit

---
+++ leesim/boss.php
@@ -0,0 +1,14 @@
+<!DOCTYPE>
+<html>
+<head>
+<title>Again</title>
+</head>
+<body>
+<?php
+$conn=mysql_connect('localhost','root','tavonga1');
+if(!$conn){
+	die("Connection Error");
+}
+?>
+</body>
+</html>
